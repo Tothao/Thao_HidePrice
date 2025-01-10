44c36c0efbb0af9e8db7e1be084b07270064900e
Thaott <tothao2502@gmail.com>
2025-01-10 16:45:37 +0700

    add attrubute hide_price_customer_group

---
+++ Setup/Patch/Data/AddHidePriceCustomerGroupAttribute.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Thao\HidePrice\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddHidePriceCustomerGroupAttribute implements DataPatchInterface
+{
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory          $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        // Tạo attribute 'hide_price_customer_group'
+        $eavSetup->addAttribute('catalog_product', 'hide_price_customer_group', [
+            'type' => 'text',
+            'backend' => '',
+            'frontend' => '',
+            'label' => 'Hide Price for Customer Groups',
+            'input' => 'multiselect',
+            'class' => '',
+            'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Table', // Sử dụng bảng cho dữ liệu multiselect
+            'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+            'visible' => true,
+            'required' => false,
+            'user_defined' => true,
+            'default' => '',
+            'searchable' => false,
+            'filterable' => false,
+            'comparable' => false,
+            'visible_on_front' => false,
+            'used_in_product_listing' => false,
+            'unique' => false,
+            'apply_to' => '',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
